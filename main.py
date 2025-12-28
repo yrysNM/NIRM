c70b7e2f3b4dae2cc8f4cf41a1e209f9e3f42a77
yrysNM <yrys.n3m@gmail.com>
2025-12-28 15:39:57 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests; 
+
+url = 'https://cvparser.ai/api/v4/parse'
+headers = {
+    'Content-Type': 'application/json',
+    'X-API-Key': 'f7e4d3c3cb17cea1b1d0362fb45685b6'
+}
+data = {
+    'url': 'https://docs.google.com/document/d/10YXNeHiTXJaInJ0PoXqORr7Qk4HeySbb/export?format=pdf'
+}
+
+response = requests.post(url, headers=headers, json=data)
+print(response.json())
